05eff979b49ec2535833f7f6067e2fe9728bfd7f
MathiasGN <43538343+math964g@users.noreply.github.com>
2020-02-26 14:38:32 +0100

    Project cards basic functions done

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,19 @@
 console.log("Javascript initiated...");
+
+let hb = document.getElementsByClassName("item");
+for (var i = 0; i < hb.length; i++) {
+  hb[i].addEventListener("mouseover", showText);
+  hb[i].addEventListener("mouseout", hideText);
+}
+
+
+function showText() {
+  console.log("showing text");
+  console.log(this);
+  this.style.fontSize = "32px";
+}
+
+function hideText() {
+  console.log("hidingtext");
+  this.style.fontSize = "0";
+}
