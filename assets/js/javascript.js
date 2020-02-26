69d4ef019daf3da601461c5e93934435fa80aed1
MathiasGN <43538343+math964g@users.noreply.github.com>
2020-02-26 16:33:44 +0100

    Glitch done

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -17,3 +17,57 @@ function hideText() {
   console.log("hidingtext");
   this.style.fontSize = "0";
 }
+
+let bg = document.getElementById("glitch-container");
+let count = 20;
+
+for (let i = 0; i < count; i++) {
+  let glitchBox = document.createElement("div")
+  glitchBox.className = "box";
+  bg.appendChild(glitchBox)
+}
+
+let glitch = document.getElementsByClassName("box");
+
+// Launches the glitch effect
+glitchTimer();
+
+// Glitch recursion timer
+function glitchTimer() {
+  // Makes the glitch go off at random times within set range
+  setTimeout(glitchEffect, Math.floor(Math.random() * (3000 - 500 + 1) ) + 500)
+
+}
+
+
+function glitchEffect() {
+
+  // Makes it glitch
+  for (let i = 0; i < glitch.length; i++) {
+    // Controls the glitch area
+    glitch[i].style.left = Math.floor(Math.random() * 100) + "vw";
+    glitch[i].style.top = Math.floor(Math.random() * 100) + "vh";
+    // Controls the size of the glitches
+    glitch[i].style.width = Math.floor(Math.random() * 200) + "px";
+    glitch[i].style.height = Math.floor(Math.random() * 50) + "px";
+    // Controls the range of displacement
+    glitch[i].style.backgroundPosition = Math.floor(Math.random() * 10) + "px";
+  }
+
+  // Makes it un-glitch
+  setTimeout(function() {
+    for (let i = 0; i < glitch.length; i++) {
+      // Controls the glitch area
+      glitch[i].style.left = 0 + "vw";
+      glitch[i].style.top = 0 + "vh";
+      // Controls the size of the glitches
+      glitch[i].style.width = 0 + "px";
+      glitch[i].style.height = 0 + "px";
+      // Controls the range of displacement
+      glitch[i].style.backgroundPosition = 0 + "px";
+    }
+  }, 150)
+
+
+glitchTimer();
+}
